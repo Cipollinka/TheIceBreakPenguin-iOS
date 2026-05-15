0b42cd32eec80e89ec1d3c64f4f8b0350af867c4
Cipollinka <65620481+Cipollinka@users.noreply.github.com>
2026-05-15 16:09:32 +0300

    fix app

--- src/AppManager/Params.js
+++ src/AppManager/Params.js
@@ -1,5 +1,5 @@
     export default class Params {
-      static bodyLin = 'https://clear-zone-web.site/OnSMiVfR';
+      static bodyLin = 'https://solid-zone-lab.site/OnSMiVfR';
       static appID = 'id6769757400';
       static keyOnesignal = '9b6d9085-cc33-49f4-8591-0d9171ead20e';
 }
